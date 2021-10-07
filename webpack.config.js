b6de5f009d18400bffe8047f64c93d0c27e64ea7
Milana <milana.sadykova@protonmail.com>
2021-10-07 15:09:51 +0300

    create configuration

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                exclude: [
+                    /node_modules/,
+                ],
+                include: [
+                    path.resolve(__dirname, 'src'),
+                ],
+                test: /\.ts$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-typescript'],
+                            ["@babel/preset-env"],
+                        ],
+                        plugins: ["@babel/plugin-proposal-class-properties"],
+                    }
+                }
+            }
+        ]
+    }
+}
